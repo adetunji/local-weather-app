f60b83d0b384d3a2bae65adfb711a66d9bf69cba
adetunji <adecorelabs@gmail.com>
2018-11-05 20:48:52 -0500

    Displayed weather data in UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { BrowserModule } from '@angular/platform-browser'
 import 'hammerjs'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component'
+import { CitySearchComponent } from './city-search/city-search.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 import { WeatherService } from './weather/weather.service';
 import { MaterialModule } from './/material.module'
 import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule],
+  declarations: [AppComponent, CurrentWeatherComponent, CitySearchComponent],
+  imports: [FormsModule, ReactiveFormsModule, BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
